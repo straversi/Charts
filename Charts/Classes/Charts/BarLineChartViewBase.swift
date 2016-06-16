5cf65571d800da28fdfd7f0f93cd58cb8530aa03
Steven Traversi <straversi@berkeley.edu>
2016-06-15 22:33:36 -0700

    Update BarLineChartViewBase.swift

--- Charts/Classes/Charts/BarLineChartViewBase.swift
+++ Charts/Classes/Charts/BarLineChartViewBase.swift
@@ -112,12 +112,12 @@ public class BarLineChartViewBase: ChartViewBase, BarLineScatterCandleBubbleChar
         
         self.highlighter = ChartHighlighter(chart: self)
         
-        _tapGestureRecognizer = NSUITapGestureRecognizer(target: self, action: #selector(BarLineChartViewBase.tapGestureRecognized(_:)))
-        _doubleTapGestureRecognizer = NSUITapGestureRecognizer(target: self, action: #selector(BarLineChartViewBase.doubleTapGestureRecognized(_:)))
-        _doubleTapGestureRecognizer.nsuiNumberOfTapsRequired = 2
-        _panGestureRecognizer = NSUIPanGestureRecognizer(target: self, action: #selector(BarLineChartViewBase.panGestureRecognized(_:)))
-        
         // I want my own damn gesture recognizers thank you very much
+        // _tapGestureRecognizer = NSUITapGestureRecognizer(target: self, action: #selector(BarLineChartViewBase.tapGestureRecognized(_:)))
+        // _doubleTapGestureRecognizer = NSUITapGestureRecognizer(target: self, action: #selector(BarLineChartViewBase.doubleTapGestureRecognized(_:)))
+        // _doubleTapGestureRecognizer.nsuiNumberOfTapsRequired = 2
+        // _panGestureRecognizer = NSUIPanGestureRecognizer(target: self, action: #selector(BarLineChartViewBase.panGestureRecognized(_:)))
+        
         // _panGestureRecognizer.delegate = self
         
         // self.addGestureRecognizer(_tapGestureRecognizer)
